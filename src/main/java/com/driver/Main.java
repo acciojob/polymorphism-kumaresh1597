ee521fee536521c41b7eceaa94c21e0db4d73b48
kumaresh1597 <kumaresh.1597@gmail.com>
2023-05-11 09:57:16 +0530

    addChanges

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,28 +2,33 @@ package com.driver;
 
 public class Main {
 
+    //Task 2
     static class Product{
 
+        // Task 3
         public int product(int x,int y){
             return x * y;
         }
 
+        // Task 4
         public int product(int x,int y,int z){
             return x*y*z;
         }
 
+        //Task 5
         public double product(double x,double y){
             return (double) x*y;
         }
     }
     public static void main(String[] args) {
+        //Task 2
           Product p = new Product();
-         int a =  p.product(2,3);
-         int b =  p.product(2,3,4);
-         double c =  p.product(2.2,3.6);
-
-        System.out.println(a+" "+b+" "+c);
+           // Task 3
+        System.out.println(p.product(2,3));
+           // Task 4
+        System.out.println(p.product(2,3,4));
+           // Task 5
+        System.out.println(p.product(2.2,3.6));
     }
-
 
 }
